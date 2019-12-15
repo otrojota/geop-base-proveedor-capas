daffef3c1ca55164205194394ac928df8ed3333d
Jorge Jimenez <jota@MacJota.local>
2019-12-14 22:21:40 -0300

    uodt

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {};
+module.exports.ProveedorCapas = require("./lib/ProveedorCapas");
+module.exports.Origen = require("./lib/Origen");
+module.exports.CapaRaster = require("./lib/Capa").CapaRaster;
+module.exports.CapaVectorial = require("./lib/Capa").CapaVectorial;
