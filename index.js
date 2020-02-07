05f37a6b6a098febb352ff6ab898cea8db00a364
Jorge Jimenez <jota@MacJota.local>
2020-02-06 23:59:22 -0300

    updt

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {};
 module.exports.ProveedorCapas = require("./lib/ProveedorCapas");
 module.exports.Origen = require("./lib/Origen");
 module.exports.CapaRaster = require("./lib/Capa").CapaRaster;
+module.exports.CapaObjetosConDatos = require("./lib/Capa").CapaObjetosConDatos;
 module.exports.CapaVectorial = require("./lib/Capa").CapaVectorial;
